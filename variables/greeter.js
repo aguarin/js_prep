f0c7ef4e3069480bb6149bfad3ae1184a0858caa
aguarin <guarin.angelo@gmail.com>
2023-05-03 17:08:04 -0400

    Add variables

---
+++ variables/greeter.js
@@ -0,0 +1,7 @@
+let firstName = 'Victor';
+console.log(`Good Morning, ${firstName}.`);
+console.log(`Good Afternoon, ${firstName}.`);
+console.log(`Good Evening, ${firstName}.`);
+console.log('Good Morning, ' + firstName + '.' );
+console.log('Good Afternoon, ' + firstName + '.' );
+console.log('Good Evening, ' + firstName + '.' );
